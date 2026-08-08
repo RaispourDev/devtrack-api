546a587698c3f5063a668ab0a2017a836bb2f016
RaispourDev <mahmood.raispour@gmail.com>
2026-08-08 13:20:05 +0330

    new table and relating database tabels
    the new table named tasks is created and related to the projects tabel

---
+++ prisma/migrations/20260808092808_add_tasks/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "projectId" INTEGER NOT NULL,
+    CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
